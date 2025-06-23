8783c073e57875aaef3f8f3ddd22007f6489a0c8
Eman <emanabukhater2001@gmail.com>
2025-06-23 20:15:01 +0300

    move the frontend and backend folders to the root folder

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-red-100 flex items-center justify-center">
-      <h1 className="text-xl text-blue-700 font-bold">Tailwind is working!</h1>
-    </div>
-  );
-}
