80ee86de159b3e1d64b98d248754d9b3f95553d0
Eman <emanabukhater2001@gmail.com>
2025-06-23 22:24:15 +0300

    Add server.js Setup

--- devbunker-backend/server.js
+++ devbunker-backend/server.js
@@ -2,17 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config(); // Load .env
 
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(` Server running on http://localhost:${PORT}`);
