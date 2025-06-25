d2f53088694a4b25fb63c363e64358fa86b134df
Eman <emanabukhater2001@gmail.com>
2025-06-25 15:58:15 +0300

    include and export isAdmin inside authMiddleware

--- devbunker-backend/middleware/authMiddleware.js
+++ devbunker-backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 
-exports.verifyToken = (req, res, next) => {
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -11,9 +12,20 @@ exports.verifyToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // attach user payload to request
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
+
+//  Middleware to check if user is admin
+const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied. Admins only." });
+  }
+  next();
+};
+
+// Export both middlewares
+module.exports = { verifyToken, isAdmin };
