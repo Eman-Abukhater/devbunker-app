110086d9b68eb8b65e53aed04927c4826ea5eba4
Eman <emanabukhater2001@gmail.com>
2025-06-25 15:52:04 +0300

    Add the admin routes to your server.js

--- devbunker-backend/server.js
+++ devbunker-backend/server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const authRoutes = require("./routes/authRoutes");
+const adminRoutes = require("./routes/adminRoutes"); 
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
